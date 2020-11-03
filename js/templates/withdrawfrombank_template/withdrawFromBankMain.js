2ff342f8499656667bb0af3b1648924e9d9d89d8
kharpuds <66372058+kharpuds@users.noreply.github.com>
2020-11-04 01:40:45 +0530

    Socketized withdraw from bank

--- js/templates/withdrawfrombank_template/withdrawFromBankMain.js
+++ js/templates/withdrawfrombank_template/withdrawFromBankMain.js
@@ -1,52 +1,66 @@
-ubsApp.getWithdrawFromBankTemplate=function(templateConfig,tempVar){
-    var object={};
-	object.color=userArray[playerChance].getplayerColor();
-	object.cash=userArray[playerChance].getplayerScore();
-	object.bankBalance=userArray[playerChance].getBankBalance();
-		ubsApp.openedTransferScenario = true;
+ubsApp.getWithdrawFromBankTemplate = function (templateConfig, tempVar) {
+    var object = {};
+    object.color = userArray[playerChance].getplayerColor();
+    object.cash = userArray[playerChance].getplayerScore();
+    object.bankBalance = userArray[playerChance].getBankBalance();
+    ubsApp.openedTransferScenario = true;
 
-	templateConfig=$.extend(templateConfig,object);
-	tempVar.html+=ubsPayOffTemplate(templateConfig);
+    templateConfig = $.extend(templateConfig, object);
+    tempVar.html += ubsPayOffTemplate(templateConfig);
 }
 
-ubsApp.openWithdrawFromBank=function(){
-	ubsApp.startCurrentScenario();
-	ubsApp.renderPageByName("withdrawFromBank");
+ubsApp.openWithdrawFromBank = function () {
+    socket.emit("serverOpenWithdrawFromBank", { roomCode: userArray[playerChance].getRoomCode() })
 }
 
-ubsApp.withdrawFromBank=function(questionId){
+socket.on('clientOpenWithdrawFromBank', function () {
+    ubsApp.startCurrentScenario();
+    ubsApp.renderPageByName("withdrawFromBank");
+})
+
+
+ubsApp.withdrawFromBank = function (questionId) {
+    socket.emit('serverWithdrawFromBank', {
+        roomCode: userArray[playerChance].getRoomCode(),
+        questionId: questionId
+    })
+}
+
+socket.on('clientWithdrawFromBank', function (data) {
+    ubsApp.socketWithdrawFromBank(data.questionId);
+})
+
+ubsApp.socketWithdrawFromBank = function (questionId) {
     console.log("Withdraw Id is : " + questionId);
-    var number =parseInt(document.getElementById("debtPaymentText").value);
+    var number = parseInt(document.getElementById("debtPaymentText").value);
     var date = new Date();
-    var startTime=date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-    ubsApp.updateScoreInDB(userArray[playerChance].getplayerStudentId(),questionId,number, 0,0, startTime,"withdrawFromBank");
-
-    if(number>0)
-    {
-        if(number<=userArray[playerChance].getBankBalance())
-        {   
-            userArray[playerChance].setplayerScore(userArray[playerChance].getplayerScore()+number);
-            userArray[playerChance].setBankBalance(userArray[playerChance].getBankBalance()-number);
+    var startTime = date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    ubsApp.updateScoreInDB(userArray[playerChance].getplayerStudentId(), questionId, number, 0, 0, startTime, "withdrawFromBank");
+    console.log("current player is: " + userArray[playerChance].getplayerName());
+    if (number > 0) {
+        if (number <= userArray[playerChance].getBankBalance()) {
+            userArray[playerChance].setplayerScore(userArray[playerChance].getplayerScore() + number);
+            userArray[playerChance].setBankBalance(userArray[playerChance].getBankBalance() - number);
             ubsApp.closeCurrentScenario();
             ubsApp.currentPlayerContents();
             ubsApp.openResultPopup({
-                        "message" : ubsApp.getTranslation("withdrawSuccessMsg").replace("{{withdrawAmount}}",number),
-                        "header" : ubsApp.getTranslation("SUCCESS"),
-                        "headerStyle" : "text-align: center;  color: black; font-weight: 700;",
-                        "buttons":[
-                            {
-                                'id':"closePopupButton",
-                                'name' : ubsApp.getTranslation("CLOSE"),
-                                'action': "ubsApp.callServerClosePopup();"
-                            }
-                        ]
-             });
+                "message": ubsApp.getTranslation("withdrawSuccessMsg").replace("{{withdrawAmount}}", number),
+                "header": ubsApp.getTranslation("SUCCESS"),
+                "headerStyle": "text-align: center;  color: black; font-weight: 700;",
+                "buttons": [
+                    {
+                        'id': "closePopupButton",
+                        'name': ubsApp.getTranslation("CLOSE"),
+                        'action': "ubsApp.callServerClosePopup();"
+                    }
+                ]
+            });
         }
-        else{
-            document.getElementById("result").innerHTML=ubsApp.translation["bankBalance"];
+        else {
+            document.getElementById("result").innerHTML = ubsApp.translation["bankBalance"];
         }
     }
-    else{
-        document.getElementById("result").innerHTML=ubsApp.translation["validAmount"];
+    else {
+        document.getElementById("result").innerHTML = ubsApp.translation["validAmount"];
     }
 }
--- server.js
+++ server.js
@@ -431,6 +431,16 @@ io.on('connection', function (socket) {
         
     });
 
+    socket.on('serverOpenWithdrawFromBank', function (data) {
+        socket.emit('clientOpenWithdrawFromBank');
+        socket.in(Number(data.roomCode)).emit('clientOpenWithdrawFromBank');
+    });
+
+    socket.on('serverWithdrawFromBank', function (data) {
+        socket.emit('clientWithdrawFromBank', { questionId: data.questionId });
+        socket.in(Number(data.roomCode)).emit('clientWithdrawFromBank', { questionId: data.questionId });
+    });
+
 })
 
 http.listen(3000, function () {
